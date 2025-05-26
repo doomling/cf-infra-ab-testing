b9ac75b9fe5727e458cb685ea881e43eb286cfd1
doomling <bethlejem@gmail.com>
2025-05-26 12:13:10 -0300

    feat/add variant

---
+++ middleware.tsx
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("variant")?.value;
+
+  if (cookie === "A") {
+    return NextResponse.next();
+  }
+
+  if (cookie === "B") {
+    return NextResponse.rewrite(new URL("/variant-b", request.url));
+  }
+
+  const variant = Math.random() < 0.5 ? "A" : "B";
+  const response =
+    variant === "A"
+      ? NextResponse.next()
+      : NextResponse.rewrite(new URL("/variant-b", request.url));
+
+  console.log(variant);
+
+  response.cookies.set("variant", variant, {
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30, // 30 días
+  });
+
+  return response;
+}
